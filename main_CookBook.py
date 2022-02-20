7693f9365c1fcf65c31a382683a7cb4740b1f49f
Borisonix <b.sharapov.nvk@gmail.com>
2022-02-20 20:05:29 +0700

    Initial commit

---
+++ main_CookBook.py
@@ -0,0 +1,13 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.event import EventDispatcher
+from kivy.properties import *
+
+class MyApp(App):
+    def build(self):
+        self.title = 'Моё приложение'
+        return Label(text="Привет!!!")
+
+if __name__ == '__main__':
+    MyApp().run()
