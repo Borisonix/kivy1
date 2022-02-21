be7e413330d231e41705f618a82798a3d82245e2
Boris <b.sharapov.nvk@gmail.com>
2022-02-21 14:31:26 +0700

    Связь KV и Python

--- main_CookBook.py
+++ main_CookBook.py
@@ -2,13 +2,10 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.event import EventDispatcher
-from kivy.properties import *
 
 class MyApp(App):
     def build(self):
-        self.title = 'Моё приложение'
-        return Label(text="Привет!!!")
+        return Label()
 
 if __name__ == '__main__':
     MyApp().run()
