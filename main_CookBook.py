58eb317565e7f6b58aad4faf5b9583c87aa0564f
Borisonix <b.sharapov.nvk@gmail.com>
2022-02-27 22:06:00 +0700

    Начата разработка приложения Weather

--- main_CookBook.py
+++ main_CookBook.py
@@ -2,10 +2,13 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from  kivy.uix.textinput import TextInput
+
 
 class MyApp(App):
     def build(self):
-        return Label()
+        pass
 
 if __name__ == '__main__':
     MyApp().run()
