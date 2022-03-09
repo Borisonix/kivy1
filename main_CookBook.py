0bc1fb3f0c7f4d63497de2334771bd12881eac9c
Boris <b.sharapov.nvk@gmail.com>
2022-03-09 16:43:52 +0700

    Недоделанный

--- main_CookBook.py
+++ main_CookBook.py
@@ -1,4 +1,3 @@
-
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -6,9 +5,11 @@ from kivy.uix.button import Button
 from  kivy.uix.textinput import TextInput
 
 
+
 class MyApp(App):
     def build(self):
         pass
+
 
 if __name__ == '__main__':
     MyApp().run()
--- main_Weather.py
+++ main_Weather.py
@@ -1,13 +1,17 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy. import ObjectProperty
 
 
 class AddLocationForm(BoxLayout):
+    search_input =
     def search_location(self):
-        print("Явное лучше, чем неявное.")
+        print("The user searched for '{}'".format(self.search_input.text))
+
 
 class WeatherApp(App):
     pass
+
 
 if __name__ == '__main__':
     WeatherApp().run()
