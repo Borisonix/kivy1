d87db54e6df31f87a2c93b6d0150409b367f93f5
Борис с нового ноута <b.sharapov.nvk@gmail.com>
2022-04-28 00:04:38 +0700

    добавлен tk_test

---
+++ tk/test_tk.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+def changeBut(event, button):
+    # if button.cget("bg") == "white":
+    if button["bg"] == "white":
+        button.configure(bg="black")
+        button.configure(fg="black")
+    else:
+        button.configure(bg="white")
+        button.configure(fg="white")
+    i, j = button["text"].split('_')
+    print(i, j)
+
+
+root = Tk()
+
+for i in range(10):
+    for j in range(10):
+        but = Button(root,
+                     borderwidth=1,
+                     bg="white",
+                     fg="white")
+        but["text"] = str(i) + "_" + str(j)
+        but.bind("<Button-1>", lambda event, but=but: changeBut(event, but))
+        but.grid(row=i, column=j)
+
+root.mainloop()
